f3ffa9160391304a461d1aa000f55a4d5bab4eaa
Jason Tuttle <jason@hearspotbark.com>
2017-06-08 16:01:38 -0400

    workin

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ for (let i = 0; i < formData.length; i++) {
   inputDiv.classList.add('form-item');
   form.appendChild(inputDiv);
   var currField = formData[i];
+  var icon = document.createElement('i');
+  let iconLabel = currField.icon;
+  icon.setAttribute("class", iconLabel);
+  inputDiv.appendChild(icon);
   if ((currField.type != 'textarea') && (currField.type != 'select')) {
     var newField = document.createElement('input');
     newField.setAttribute("type", currField.type);
@@ -19,12 +23,14 @@ for (let i = 0; i < formData.length; i++) {
     var newTextArea = document.createElement('textarea');
     newTextArea.setAttribute("value", currField.label);
     newTextArea.setAttribute("id", currField.id);
+    newTextArea.classList.add('form-input');
     inputDiv.appendChild(newTextArea);
   }
   if (currField.type === 'select') {
     var newSelect = document.createElement('select');
     newSelect.setAttribute("value", currField.label);
     newSelect.setAttribute("id", currField.id);
+    newSelect.classList.add('form-input');
     for (let j = 0; j < currField.options.length; j++) {
       var selectOption = document.createElement('option');
       selectOption.setAttribute("value", currField.options[j].value);
